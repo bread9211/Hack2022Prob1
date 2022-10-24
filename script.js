aa419eddb3298f0d3dbaaec3e0ec2795bdf3962e
bread9211 <sunnyfang921@gmail.com>
2022-10-24 14:17:28 -0700

    a

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const BingMapsKey = 'ff9ba92f56ed4ed0b1edf0bc8a23b311'
 function getLoc() {
     var location
     navigator.geolocation.getCurrentPosition((function(pos) {location = pos.coords}).bind(window))
-    console.。log(location)
+    console.log(location)
     return location
 }
 
