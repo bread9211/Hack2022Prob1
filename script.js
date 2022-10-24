49a213192e0aaa38968b49036a5a270758d6b25f
bread9211 <sunnyfang921@gmail.com>
2022-10-24 14:08:29 -0700

    test on https

---
+++ script.js
@@ -0,0 +1,12 @@
+const BingMapsKey = 'ff9ba92f56ed4ed0b1edf0bc8a23b311'
+
+function getLoc() {
+    var location
+    navigator.geolocation.getCurrentPosition((function(pos) {location = pos.coords}).bind(window))
+    console.log(location)
+    return location
+}
+
+function get() {
+    
+}
